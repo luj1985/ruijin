f29240414c2487c51a22e1d3fe2a08346db7b0d7
Lu, Jun <luj1985@gmail.com>
2016-08-24 22:27:51 +0800

    update asset

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 const vips = [
-  { name: "殿塚 亮祐", img: "1殿塚 亮祐.jpg", detail: "详细-33.jpg" },
-  { name: "龚彪", img: "2龚彪.jpg", detail: "详细-34.jpg" },
-  { name: "郭学刚", img: "3郭学刚.jpg", detail: "详细-35.jpg" },
-  { name: "令狐恩强", img: "4令狐恩强.jpg", detail: "详细-36.jpg" },
+  { name: "殿塚 亮祐", img: "1殿塚 亮祐.jpg", detail: "1.jpg" },
+  { name: "龚彪", img: "2龚彪.jpg", detail: "2.jpg" },
+  { name: "郭学刚", img: "3郭学刚.jpg", detail: "3.jpg" },
+  { name: "令狐恩强", img: "4令狐恩强.jpg", detail: "4.jpg" },
 //  { name: "刘凯", img: "5刘凯.jpg", detail: "详细-37.jpg" },
-  { name: "冀明", img: "6冀明.jpg", detail: "详细-38.jpg" },
+  { name: "冀明", img: "6冀明.jpg", detail: "5.jpg" },
 //  { name: "麻树人", img: "7麻树人.jpg", detail: "详细-39.jpg" },
-  { name: "土屋 貴愛", img: "8土屋 貴愛.jpg", detail: "详细-40.jpg" },
+  { name: "土屋 貴愛", img: "8土屋 貴愛.jpg", detail: "6.jpg" },
 //  { name: "李巍", img: "9李巍.jpg", detail: "详细-41.jpg" },
-  { name: "李文", img: "10李文.jpg", detail: "详细-42.jpg" },
-  { name: "李延青", img: "11李延青.jpg", detail: "详细-43.jpg" },
-  { name: "李兆申", img: "12李兆申.jpg", detail: "详细-44.jpg" },
+  { name: "李文", img: "10李文.jpg", detail: "7.jpg" },
+  { name: "李延青", img: "11李延青.jpg", detail: "8.jpg" },
+  { name: "李兆申", img: "12李兆申.jpg", detail: "9.jpg" },
 //  { name: "席惠君", img: "13席惠君.jpg", detail: "详细-45.jpg" },
-  { name: "于君", img: "14于君.jpg", detail: "详细-46.jpg" },
-  { name: "于涛", img: "15于涛.jpg", detail: "详细-47.jpg" },
-  { name: "张澍田", img: "16张澍田.jpg", detail: "详细-48.jpg" },
-  { name: "张筱凤", img: "17张筱凤.jpg", detail: "详细-49.jpg" }
+  { name: "于君", img: "14于君.jpg", detail: "10.jpg" },
+  { name: "于涛", img: "15于涛.jpg", detail: "11.jpg" },
+  { name: "张澍田", img: "16张澍田.jpg", detail: "12.jpg" },
+  { name: "张筱凤", img: "17张筱凤.jpg", detail: "13.jpg" }
 ];
 
 const nurses = [
