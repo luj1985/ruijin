84a38b9dad7786082d4e779dd5ac1307bd75d3cd
Lu, Jun <luj1985@gmail.com>
2016-08-28 11:11:17 +0800

    update assets

--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,11 @@ const photos = [
     "0U4A5292.jpg", "0U4A5315.jpg", "0U4A5339.jpg", "0U4A5416.jpg", "DSC_4036.jpg", "DSC_4083.jpg", "DSC_4126.jpg", "DSC_4200.jpg", "DSC_4222.jpg",
     "0U4A5314.jpg", "0U4A5323.jpg", "0U4A5356.jpg", "DSC_4003.jpg", "DSC_4055.jpg", "DSC_4096.jpg", "DSC_4170.jpg", "DSC_4202.jpg", "DSC_4232.jpg"
   ] },
-  { text : "8月27日", name: "8月27日", detail : [] },
+  { text : "8月27日", name: "8月27日", detail : [
+    "0U4A5470.jpg",  "0U4A5508.jpg",  "0U4A5610.jpg",  "0U4A5896.jpg",  "IMG_9029.jpg",  "IMG_9161.jpg",  "IMG_9235.jpg",  "IMG_9384.jpg",  "IMG_9479.jpg",
+    "0U4A5472.jpg",  "0U4A5591.jpg",  "0U4A5671.jpg",  "IMG_8897.jpg",  "IMG_9035.jpg",  "IMG_9192.jpg",  "IMG_9313.jpg",  "IMG_9405.jpg",
+    "0U4A5494.jpg",  "0U4A5604.jpg",  "0U4A5882.jpg",  "IMG_8945.jpg",  "IMG_9142.jpg",  "IMG_9226.jpg",  "IMG_9337.jpg",  "IMG_9443.jpg"
+  ] },
   { text : "8月28日", name: "8月28日", detail : [] }
 ]
 
